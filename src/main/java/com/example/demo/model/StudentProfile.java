8ea0b3ea2201cdf684f1b2c02dbfb1f2fc4e4342
bala-amypo <bala@amypo.in>
2025-12-17 06:27:11 +0000

    User commit

--- src/main/java/com/example/demo/model/StudentProfile.java
+++ src/main/java/com/example/demo/model/StudentProfile.java
@@ -26,6 +26,59 @@ public class StudentProfile{
     
     private LocalDateTime createdAt=LocalDateTime.now();
     
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+  
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public Integer getYearLevel() {
+        return yearLevel;
+    }
+    public void setYearLevel(Integer yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+
+    public Boolean isActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
     
 }
